3b8fd941a4bbb9ef1a6c8bc85b75f92499e59f3d
AMiggel <antonnio762@gmail.com>
2018-08-30 12:08:50 -0500

    first commit

---
+++ app/src/main/java/co/com/unac/susalud/MainActivity.java
@@ -0,0 +1,13 @@
+package co.com.unac.susalud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
