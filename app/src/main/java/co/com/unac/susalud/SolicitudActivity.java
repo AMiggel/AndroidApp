d1a516a013edfca2e7fab57da6cbe630ff0b6721
Antonio Marin <antonnio762@gmail.com>
2018-09-04 20:17:45 -0500

    Activitys
    Se agregan vistas, de informacion de la cita, y solicitar

---
+++ app/src/main/java/co/com/unac/susalud/SolicitudActivity.java
@@ -0,0 +1,13 @@
+package co.com.unac.susalud;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SolicitudActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.solicitud_activity);
+    }
+}
--- app/src/main/java/co/com/unac/susalud/WelcomeActivity.java
+++ app/src/main/java/co/com/unac/susalud/WelcomeActivity.java
@@ -3,7 +3,7 @@ package co.com.unac.susalud;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
