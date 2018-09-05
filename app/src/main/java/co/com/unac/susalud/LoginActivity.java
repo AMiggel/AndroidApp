3807d276220638eaa6cc1f37e039146247d070bf
Antonio Marin <antonnio762@gmail.com>
2018-09-04 20:54:13 -0500

    onClickListener

--- app/src/main/java/co/com/unac/susalud/LoginActivity.java
+++ app/src/main/java/co/com/unac/susalud/LoginActivity.java
@@ -1,7 +1,9 @@
 package co.com.unac.susalud;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+    }
+
+    public void ingresar(View view){
+        Intent iniciarSesion= new Intent(LoginActivity.this,WelcomeActivity.class);
+
     }
 }
