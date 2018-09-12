476a60bf3c98947ce971a95ee54d44668226ed05
Antonio Miguel <antonnio762@gmail.com>
2018-09-11 20:38:54 -0500

    Consumiendo API Citas

--- app/src/main/java/co/com/unac/susalud/vista/CitasActivity.java
+++ app/src/main/java/co/com/unac/susalud/vista/CitasActivity.java
@@ -2,14 +2,45 @@ package co.com.unac.susalud.vista;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.List;
 
 import co.com.unac.susalud.R;
+import co.com.unac.susalud.datasource.CitaDataSource;
+import co.com.unac.susalud.dominio.CitaDominio;
+import co.com.unac.susalud.modelo.Cita;
+import co.com.unac.susalud.vista.adapter.CitaAdapter;
 
-public class CitasActivity extends AppCompatActivity {
+public class CitasActivity extends AppCompatActivity implements CitaDominio.View {
+
+    private CitaAdapter citaAdapter;
+    private CitaDominio citaDominio;
+    private RecyclerView lista;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.citas_activity);
+        setContentView(R.layout.activity_main);
+        lista = (RecyclerView) findViewById(R.id.lista);
+        citaDominio = new CitaDominio(this, CitaDataSource.getInstance(this));
+        consultarCita();
+    }
+
+    private void consultarCita(){
+        citaDominio.consultarCitas();
+    }
+
+    @Override
+    public void showCitas(List<Cita> citas){
+        citaAdapter= new CitaAdapter(getApplicationContext(),citas,this);
+        lista.setAdapter(citaAdapter);
+    }
+
+    @Override
+    public void showMensaje(int mensaje){
+        Log.d("Error", getString(mensaje));
     }
 }
--- app/src/main/java/co/com/unac/susalud/vista/adapter/CitaAdapter.java
+++ app/src/main/java/co/com/unac/susalud/vista/adapter/CitaAdapter.java
@@ -2,10 +2,14 @@ package co.com.unac.susalud.vista.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
+import co.com.unac.susalud.R;
 import co.com.unac.susalud.modelo.Cita;
 import co.com.unac.susalud.vista.CitasActivity;
 
@@ -23,5 +27,33 @@ public class CitaAdapter extends RecyclerView.Adapter<CitaAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewTypeCita){
 
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.citas_activity, parent,false);
+            return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+    Cita item= items.get(position);
+    holder.type.setText(item.getType());
+    holder.medico.setText(item.getMedic());
+    holder.pacient.setText(item.getPacient());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static  class ViewHolder extends RecyclerView.ViewHolder{
+         public TextView type;
+         public TextView medico;
+         public TextView pacient;
+
+    public ViewHolder (final View itemView){
+        super(itemView);
+        type=(TextView) itemView.findViewById(R.id.tipoCita);
+        medico=(TextView) itemView.findViewById(R.id.nombreMedico);
+        pacient=(TextView) itemView.findViewById(R.id.horaCita);
+    }
     }
 }
